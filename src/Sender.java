91a95ba4374ff77ad6ea3bac833c4efd5e67c29b
rustleupchef <123332908+rustleupchef@users.noreply.github.com>
2023-03-30 22:44:33 -0500

    Add files via upload

---
+++ src/Sender.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+
+public class Sender {
+    public static void main(String[] args) throws Exception {
+        ServerSocket server = new ServerSocket(Integer.valueOf(getVal(1)));
+        Socket socket = server.accept();
+        socket.getOutputStream().write("You got a visitor".getBytes());
+        socket.close();
+        server.close();
+    }
+
+    private static String getVal(int index) throws Exception{
+        Scanner scanner = new Scanner(new File("Details.txt"));
+        String text = "";
+        while(scanner.hasNextLine()) {
+            text += scanner.nextLine() + "\n";
+        }
+        scanner.close();
+        scanner.close();
+        return text.split("\n")[index];
+    }
+}
